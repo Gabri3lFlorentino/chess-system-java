813e5e382b4432b29b9bdb5c058fb7df717ceefe
Gabriel Florentino <gabriel.florentino@discente.ufg.br>
2024-07-04 13:06:37 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -44,6 +44,19 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	public Piece  removerPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not pon the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece  aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
